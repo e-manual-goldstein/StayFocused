0fce1a30ef33f83b4732aa24a77128e22698c4d7
Kevin Shortall <kevinshortall@gmail.com>
2023-09-24 13:24:04 +0100

    Adding context menus to DailySummary

---
+++ StayFocused/GUI/CustomGridView.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StayFocused
+{
+    public class CustomGridView : GridView
+    {
+        private ContextMenu processNameMenu;
+        private ContextMenu timespanColumnContextMenu;
+
+        public CustomGridView() : base()
+        {
+            SetupColumnContextMenus();
+        }
+
+        private void OrderByMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FilterByMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetupColumnContextMenus()
+        {
+            processNameMenu = new ContextMenu();
+            timespanColumnContextMenu = new ContextMenu();
+
+            var filterByMenuItem = new MenuItem { Header = "Filter By" };
+            filterByMenuItem.Click += FilterByMenuItem_Click;
+
+            var orderByMenuItem = new MenuItem { Header = "Order By" };
+            orderByMenuItem.Click += OrderByMenuItem_Click;
+
+            processNameMenu.Items.Add(filterByMenuItem);
+            processNameMenu.Items.Add(orderByMenuItem);
+
+            //timespanColumnContextMenu.Items.Add(orderByMenuItem);
+            
+ 
+
+            foreach (var column in Columns)
+            {
+                
+            }
+
+            //// Assign the context menus to their respective columns
+            //NameColumnHeader.ContextMenu = nameColumnContextMenu;
+            //TimespanColumnHeader.ContextMenu = timespanColumnContextMenu;
+        }
+    }
+
+    public class CustomGridViewColumn : GridViewColumn
+    {
+
+    }
+}
--- StayFocused/GUI/DailySummary.xaml.cs
+++ StayFocused/GUI/DailySummary.xaml.cs
@@ -20,18 +20,115 @@ namespace StayFocused.GUI
     /// </summary>
     public partial class DailySummary : Window
     {
+        private ContextMenu processNameMenu;
+        private ContextMenu timespanColumnContextMenu;
+        private Grid grid;
+        private GridViewColumn[] gridViewColumns;
+
         public DailySummary()
         {
             InitializeComponent();
-
-            InitializeComponent();
-
+            SetUpViewComponents();
+            //SetupColumnContextMenus();
             DataContext = _activityViewModel = new ActivityViewModel();
+        }
 
-            //// Add sample activities
-            //var viewModel = DataContext as ActivityViewModel;
+        private void SetUpViewComponents()
+        {
+            grid = Content as Grid;
+            foreach (var child in grid.Children)
+            {
+                if (child is ListView listView)
+                {
+                    if (listView.View is GridView gridView)
+                    {
+                        foreach (var column in gridView.Columns)
+                        {
+                            SetContextMenuForColumn(column);
+                        }
+                    }
+                }
+            }
+        }
 
+        private void SetContextMenuForColumn(GridViewColumn column)
+        {
+            switch (column.Header)
+            {
+                case "Process Name":
+                    AddOrderByMenuItem(column);
+                    AddFilterByMenuItem(column);
+                    break;
+                case "Window Title":
+                    AddOrderByMenuItem(column);
+                    AddFilterByMenuItem(column);
+                    break;
+                case "Total Duration":
+                    AddOrderByMenuItem(column);
+                    break;
+                case "Is Selected":
+                    AddSelectAllMenuItem(column);
+                    AddUnselectAllMenuItem(column);
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        private void AddSelectAllMenuItem(GridViewColumn column)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddUnselectAllMenuItem(GridViewColumn column)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddOrderByMenuItem(GridViewColumn column)
+        {
+            if (!(column.Header is GridViewColumnHeader columnHeader))
+            {
+                column.Header = new GridViewColumnHeader() { Content = column.Header };
+            }
+            var contextMenu = (column.Header as GridViewColumnHeader).ContextMenu ??= new ContextMenu();
+            var filterByMenuItem = new MenuItem { Header = "Filter By" };
+            filterByMenuItem.Click += FilterByMenuItem_Click;
+            contextMenu.Items.Add(filterByMenuItem);
+        }
+
+        private void AddFilterByMenuItem(GridViewColumn column)
+        {
+            if (!(column.Header is GridViewColumnHeader columnHeader))
+            {
+                column.Header = new GridViewColumnHeader() { Content = column.Header };
+            }
+            var contextMenu = (column.Header as GridViewColumnHeader).ContextMenu ??= new ContextMenu();
+            var orderByMenuItem = new MenuItem { Header = "Order By" };
+            orderByMenuItem.Click += OrderByMenuItem_Click;
+            contextMenu.Items.Add(orderByMenuItem);
+        }
+
+        private void SetupColumnContextMenus()
+        {
+            processNameMenu = new ContextMenu();
+            timespanColumnContextMenu = new ContextMenu();
+
+            var filterByMenuItem = new MenuItem { Header = "Filter By" };
+            filterByMenuItem.Click += FilterByMenuItem_Click;
+
+            var orderByMenuItem = new MenuItem { Header = "Order By" };
+            orderByMenuItem.Click += OrderByMenuItem_Click;
+
+            processNameMenu.Items.Add(filterByMenuItem);
+            processNameMenu.Items.Add(orderByMenuItem);
+
+            //timespanColumnContextMenu.Items.Add(orderByMenuItem);
+            
+
+            //// Assign the context menus to their respective columns
+            //NameColumnHeader.ContextMenu = nameColumnContextMenu;
+            //TimespanColumnHeader.ContextMenu = timespanColumnContextMenu;
         }
 
         private ActivityViewModel _activityViewModel;
@@ -49,6 +146,16 @@ namespace StayFocused.GUI
                     TotalDuration = TimeSpan.FromMilliseconds(summary.Count() * Constants.MonitoringIntervalMilliseconds)
                 });
             }           
-        }        
+        }
+        private void OrderByMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FilterByMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
