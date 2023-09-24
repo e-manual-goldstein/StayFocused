724467be742a6563120c107ee88380358ff72be8
Kevin Shortall <kevinshortall@gmail.com>
2023-09-24 22:01:14 +0100

    started filter dialog

---
+++ StayFocused/GUI/AddFilterDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StayFocused
+{
+    /// <summary>
+    /// Interaction logic for InputDialog.xaml
+    /// </summary>
+    public partial class AddFilterDialog : Window
+    {
+        public AddFilterDialog(Type fieldType)
+        {
+            
+        }
+
+        private void OnOkClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void OnCancelClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
